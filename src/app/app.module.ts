535f2459518c5b0c2b7ffef6a8f1f4ad2d475677
Edward Llanca Haro <ellanca@belatrixsf.com>
2017-09-27 21:11:12 -0500

    add color, images, fonts and design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/tools/menu/menu.component';
+import { FooterComponent } from './components/tools/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/tools/menu/menu.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component ({
+	selector: 'menu',
+	templateUrl: 'menu.html',
+	styleUrls:   ['menu.component.css']
+})
+
+export class MenuComponent {}
